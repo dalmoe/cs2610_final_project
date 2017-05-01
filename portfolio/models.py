c1becc8a33a7ac062b07d1a4a507826a1104c3a3
Dallin Schwartz <dallin.schwartz@gmail.com>
2017-05-01 03:43:46 +0000

    Styles applied and project finished

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,17 +1,17 @@
 from __future__ import unicode_literals
-
+from django.http import HttpResponseRedirect
 from django.db import models
 from datetime import datetime
 from django.core.urlresolvers import reverse
 # Create your models here.
 class Project(models.Model):
     project_name = models.CharField(max_length=32)
-    project_description = models.TextField(default="No description")
-    project_source = models.URLField(default="default")
+    project_description = models.TextField(default="")
+    project_source = models.URLField(default="")
     pub_date = models.DateTimeField(default=datetime.now, blank=True)
 
     def get_absolute_url(self):
-        return reverse('portfolio:detail')
+        return reverse('portfolio:detail', kwargs={'pk': self.pk})
     def __str__(self):
         return self.project_name;
     
